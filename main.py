7136e37a87ecc578e9186692a87c0659c4b77eb9
Theotim <84569887+ichi05@users.noreply.github.com>
2023-11-28 12:56:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ from function import *
 import os 
 intro()
 print("Rentre la commande que tu veux entre :")
-acces=int(input("1:mots les moins importants,2:mots les plus importants,3:mot le plus reter par un president,4:les presidents ayants parler de nation,5:le premier president qui a parler du climat,6:les mots dit par tout les presidents"))
-while acces <1 or acces > 6:
-    acces=int(input("1:mots les moins importants,2:mots les plus importants,3:mot le plus reter par un president,4:les presidents ayants parler de nation,5:le premier president qui a parler du climat,6:les mots dit par tout les presidents"))
-    
-if acces ==1:
+acces=int(input("1. Mots les moins importants : \n"
+                "2. mots les plus importants : \n"
+                "3. mot le plus reter par un president : \n"
+                "4. les presidents ayants parler de nation : \n"
+                "5. le premier president qui a parler du climat : \n"
+                "6. les mots dit par tout les presidents : \n"
+                "7. Afficher TF-IDF : \n"))
+while acces <1 or acces > 7:
+    acces=int(input("1. Mots les moins importants : \n"
+                "2. mots les plus importants : \n"
+                "3. mot le plus reter par un president : \n"
+                "4. les presidents ayants parler de nation : \n"
+                "5. le premier president qui a parler du climat : \n"
+                "6. les mots dit par tout les presidents : \n"
+                "7. Afficher TF-IDF : \n"))
+if acces == 1:
     print(min_idf())
-elif acces==2 :
+elif acces == 2 :
     print(max_td_idf())
-elif acces==3 :
+elif acces == 3 :
     valeur=input("nom ou partie du nom du president souhaiter")
     print(mot_rep(valeur))
-elif acces ==4:
+elif acces == 4:
     print(Nation())
 elif acces == 5 :
     print(climat())
-"""elif acces==6:
-    print(mot_evoque())"""
+elif acces == 6:
+    print(mot_evoque())
+elif acces == 7 :
+    print(TF_idf())
